7b127d28d51f52348141b7c0a4de2b137ed631f7
Trung Thái <trungthai230700@gmail.com>
2025-11-22 16:42:46 +0700

    document.getElement.textContent

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-document.getElementById("myH1").textContent="xin chao";
-document.getElementById("myP").textContent="hello world";
+// document.getElementById("myH1").textContent="xin chao";
+// document.getElementById("myP").textContent="hello world";
+// let age = 25;
+// let name = "Trung Thai";
+// let tuvung = "단가";
+// console.log(`nam nay ${age} tuoi`);
+// console.log(`tôi tên là ${name}`);
+// console.log(`từ vựng tiếng Hàn hôm nay là ${tuvung}`)
+
+let fullName = "Ho Trung Thai";
+let age = 25;
+let GPA = 2.86;
+    document.getElementById("p1").textContent= fullName;
+    document.getElementById(`p2`).textContent = age;
+    document.getElementById(`p3`).textContent = GPA;
